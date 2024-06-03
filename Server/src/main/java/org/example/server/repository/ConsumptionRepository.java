2bbaaeb96feea6283ea3d94299548719e6edc36f
beth23eli <secrieru.eliza@gmail.com>
2024-06-03 21:08:34 +0300

    added prediction functions

--- Server/src/main/java/org/example/server/repository/ConsumptionRepository.java
+++ Server/src/main/java/org/example/server/repository/ConsumptionRepository.java
@@ -12,6 +12,6 @@ import java.util.List;
 
 @Repository
 public interface ConsumptionRepository extends JpaRepository<Consumption, Integer> {
-
+    List<Consumption> findByDepartmentId(Integer departmentId);
     List<Consumption> findByDepartmentIdAndConsumptionDateBetween(Integer departmentId, LocalDate startDate, LocalDate endDate);
 }
